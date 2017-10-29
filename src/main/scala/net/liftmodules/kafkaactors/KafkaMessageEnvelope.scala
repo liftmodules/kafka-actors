adc3510c9d772d8551da15fe7f5719702c1584ed
Matt Farmer <matt@frmr.me>
2017-10-28 22:06:17 -0400

    Drop serialized manifest
    Given that we're providing type hints on everything we expect to
    consume, passing along a serialized manifest is a bit extra.

--- src/main/scala/net/liftmodules/kafkaactors/KafkaMessageEnvelope.scala
+++ src/main/scala/net/liftmodules/kafkaactors/KafkaMessageEnvelope.scala
@@ -5,39 +5,17 @@ import net.liftweb.json._
 import net.liftweb.json.Extraction._
 
 case class KafkaMessageEnvelope(
-  manifestBytes: Array[Byte],
   decomposedData: JValue
 )
 
 object KafkaMessageEnvelope {
-  implicit val formats = DefaultFormats + FullTypeHints(classOf[KafkaActorMessage] :: Nil)
+  implicit val formats = DefaultFormats + FullTypeHints(classOf[Object] :: Nil)
 
   def apply[T](data: T)(implicit manifest: Manifest[T]): KafkaMessageEnvelope = {
-    val baos = new ByteArrayOutputStream()
-    val oos = new ObjectOutputStream(baos)
-
-    try {
-      oos.writeObject(manifest)
-      val manifestBytes = baos.toByteArray()
-
-      val decomposedData = decompose(data)
-      apply(manifestBytes, decomposedData)
-    } finally {
-      oos.close()
-      baos.close()
-    }
+    apply(decompose(data))
   }
 
-  def extract[T](envelope: KafkaMessageEnvelope): T = {
-    val bais = new ByteArrayInputStream(envelope.manifestBytes)
-    val ois = new ObjectInputStream(bais)
-
-    try {
-      implicit val manifest: Manifest[T] = ois.readObject().asInstanceOf[Manifest[T]]
-      envelope.decomposedData.extract[T]
-    } finally {
-      ois.close()
-      bais.close()
-    }
+  def extract[T](envelope: KafkaMessageEnvelope)(implicit manifest: Manifest[T]): T = {
+    envelope.decomposedData.extract[T]
   }
 }
